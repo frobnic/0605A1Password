78d7274c7344267c8de95547bc6a9aa867f8c3ef
frank paulsen <frobnic+ita@gmail.com>
2019-08-27 14:20:55 +0200

    different method to generate passwords

--- 0605A1Password/Program.cs
+++ 0605A1Password/Program.cs
@@ -8,37 +8,59 @@ namespace _0605A1Password
 {
     class Program
     {
+        static Random zufall = new Random();
+
+        static char zufallschar(string zeichenmenge)
+        {
+            int len = zeichenmenge.Length;
+            int pos;
+            char[] zeichen = zeichenmenge.ToCharArray();
+
+            pos = zufall.Next(0, len);
+            return zeichen[pos];
+        }
+
         static void Main(string[] args)
         {
             string passwort = "";
+            string anderespasswort = "";
             int i;      
             Random zuf = new Random();
+
+            string kleinbuchstaben = "abcdefghijklmnopqrstuvwxyzäöüß";
+            string grobbuchstaben = kleinbuchstaben.ToUpper();
+            string ziffern = "0123456789";
+            string sonderzeichen = "<>|,.-;:_°^!\"§$%&/()=?´`+*~#'";
 
             // vier kleinbuchstaben generieren:
             for (i = 1; i < 5; i++)
             {
                 passwort = passwort + (char)zuf.Next(0x61, 0x7a);
+                anderespasswort += zufallschar(kleinbuchstaben+grobbuchstaben);
             }
-
             // einen grossbuchstaben generieren:
             for (i = 1; i < 2; i++)
             {
                 passwort = passwort + (char)zuf.Next(0x41, 0x5a);
+                anderespasswort += zufallschar(grobbuchstaben);
             }
+
 
             // zwei ziffern generieren:
             for (i = 1; i <= 2; i++)
             {
                 passwort = passwort + (char)zuf.Next(0x30, 0x39);
+                anderespasswort += zufallschar(ziffern);
             }
-
             // ein sonderzeichen generieren:
             for (i = 1; i <= 2; i++)
             {
                 passwort = passwort + (char)zuf.Next(0x21, 0x2f);
+                anderespasswort += zufallschar(sonderzeichen);
             }
 
-            Console.WriteLine("Die generierte Zeichenmenge lautet:  {0}", passwort);
+            Console.WriteLine("Die generierte  Zeichenmenge lautet: {0}", passwort);
+            Console.WriteLine("Die andere      Zeichenmenge lautet: {0}", anderespasswort);
 
             char[] c = passwort.ToArray();
             char t;
@@ -47,8 +69,8 @@ namespace _0605A1Password
             for ( i = 1; i < 10; i++)
             {
                 // zeichen irgendwie wuerfeln
-                a = zuf.Next(1, 9);
-                b = zuf.Next(1, 9);
+                a = zuf.Next(0, 9);
+                b = zuf.Next(0, 9);
 
                 t = c[a];
                 c[a] = c[b];
@@ -62,6 +84,7 @@ namespace _0605A1Password
                 Console.Write("{0}", u);
             }
 
+            Console.WriteLine();
             Console.WriteLine();
 
         }
